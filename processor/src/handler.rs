388f60843e93718152d01be5825fdc36feb9f52c
Shannyn Telander <telander@bitwise.io>
2018-08-15 18:28:10 -0500

    Create structure for payload and state in handler
    Creates the initial structure for functions dealing with the payload
    and state within the main implementation of the bond transaction
    handler, yet to be implemented.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- processor/src/handler.rs
+++ processor/src/handler.rs
@@ -19,6 +19,14 @@ use sawtooth_sdk::processor::handler::TransactionHandler;
 
 use bond_common::addressing::*;
 
+use bond_common::proto::{
+    payload_bond, payload_holding, payload_order, payload_organization, payload_participant,
+    payload_quote, payload_receipt, payload_settlement,
+};
+
+use payload::{Action, BondPayload};
+use state::BondState;
+
 pub struct BondTransactionHandler {
     family_name: String,
     family_versions: Vec<String>,
@@ -32,6 +40,114 @@ impl BondTransactionHandler {
             family_versions: vec!["0.1".to_string()],
             namespaces: vec![get_bond_namespace()],
         }
+    }
+
+    fn _create_participant(
+        &self,
+        _payload: payload_participant::CreateParticipant,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _update_participant(
+        &self,
+        _payload: payload_participant::UpdateParticipant,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _create_organization(
+        &self,
+        _payload: payload_organization::CreateOrganization,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _update_organization(
+        &self,
+        _payload: payload_organization::UpdateOrganization,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _update_organization_authorization(
+        &self,
+        _payload: payload_organization::UpdateOrganizationAuthorization,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _create_order(
+        &self,
+        _payload: payload_order::CreateOrder,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _create_quote(
+        &self,
+        _payload: payload_quote::CreateQuote,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _create_holding(
+        &self,
+        _payload: payload_holding::CreateHolding,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _delete_holding(
+        &self,
+        _payload: payload_holding::DeleteHolding,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _create_settlement(
+        &self,
+        _payload: payload_settlement::CreateSettlement,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _create_receipt(
+        &self,
+        _payload: payload_receipt::CreateReceipt,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
+    }
+
+    fn _create_bond(
+        &self,
+        _payload: payload_bond::CreateBond,
+        mut _state: BondState,
+        _signer: &str,
+    ) -> Result<(), ApplyError> {
+        unimplemented!();
     }
 }
 
@@ -50,9 +166,89 @@ impl TransactionHandler for BondTransactionHandler {
 
     fn apply(
         &self,
-        _request: &TpProcessRequest,
-        _context: &mut TransactionContext,
+        request: &TpProcessRequest,
+        context: &mut TransactionContext,
     ) -> Result<(), ApplyError> {
-        unimplemented!();
+        let payload = BondPayload::new(request.get_payload());
+
+        let payload = match payload {
+            Err(e) => return Err(e),
+            Ok(payload) => payload,
+        };
+
+        let payload = match payload {
+            Some(x) => x,
+            None => {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "Request must contain a payload",
+                )))
+            }
+        };
+
+        let signer = request.get_header().get_signer_public_key();
+        let state = BondState::new(context);
+
+        info!(
+            "Payload: {:?} {} {}",
+            payload.get_action(),
+            request.get_header().get_inputs()[0],
+            request.get_header().get_outputs()[0]
+        );
+
+        match payload.get_action() {
+            Action::CreateParticipant(create_participant_payload) => {
+                self._create_participant(create_participant_payload, state, signer)?
+            }
+
+            Action::UpdateParticipant(update_participant_payload) => {
+                self._update_participant(update_participant_payload, state, signer)?
+            }
+
+            Action::CreateOrganization(create_organization_payload) => {
+                self._create_organization(create_organization_payload, state, signer)?
+            }
+
+            Action::UpdateOrganization(update_organization_payload) => {
+                self._update_organization(update_organization_payload, state, signer)?
+            }
+
+            Action::UpdateOrganizationAuthorization(update_organization_authorization_payload) => {
+                self._update_organization_authorization(
+                    update_organization_authorization_payload,
+                    state,
+                    signer,
+                )?
+            }
+
+            Action::CreateOrder(create_order_payload) => {
+                self._create_order(create_order_payload, state, signer)?
+            }
+
+            Action::CreateQuote(create_quote_payload) => {
+                self._create_quote(create_quote_payload, state, signer)?
+            }
+
+            Action::CreateSettlement(create_settlement_payload) => {
+                self._create_settlement(create_settlement_payload, state, signer)?
+            }
+
+            Action::CreateReceipt(create_receipt_payload) => {
+                self._create_receipt(create_receipt_payload, state, signer)?
+            }
+
+            Action::CreateHolding(create_holding_payload) => {
+                self._create_holding(create_holding_payload, state, signer)?
+            }
+
+            Action::DeleteHolding(delete_holding_payload) => {
+                self._delete_holding(delete_holding_payload, state, signer)?
+            }
+
+            Action::CreateBond(create_bond_payload) => {
+                self._create_bond(create_bond_payload, state, signer)?
+            }
+        }
+
+        Ok(())
     }
 }
